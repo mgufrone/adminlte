a3faa07cd3c69bd728f1cd87632a069f2d2073b7
Mochamad Gufron <mgufronefendi@gmail.com>
2014-09-06 09:32:16 +0700

    modify header layout

--- src/views/layouts/dashboard/header.blade.php
+++ src/views/layouts/dashboard/header.blade.php
@@ -13,31 +13,72 @@
         </a>
         <div class="navbar-right">
             <ul class="nav navbar-nav">
-               
+
                 @if(Sentry::check())
+                <!-- <li class="dropdown notifications-menu">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                        <i class="fa fa-warning"></i>
+                        <span class="label label-warning">10</span>
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li class="header">You have 10 notifications</li>
+                        <li>
+                        
+                            <div style="position: relative; overflow: hidden; width: auto; height: 200px;" class="slimScrollDiv"><ul style="overflow: hidden; width: 100%; height: 200px;" class="menu">
+                                <li>
+                                    <a href="#">
+                                        <i class="ion ion-ios7-people info"></i> 5 new members joined today
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        <i class="fa fa-warning danger"></i> Very long description here that may not fit into the page and may cause design problems
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        <i class="fa fa-users warning"></i> 5 new members joined
+                                    </a>
+                                </li>
+
+                                <li>
+                                    <a href="#">
+                                        <i class="ion ion-ios7-cart success"></i> 25 sales made
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        <i class="ion ion-ios7-person danger"></i> You changed your username
+                                    </a>
+                                </li>
+                            </ul><div style="background: none repeat scroll 0% 0% rgb(0, 0, 0); width: 3px; position: absolute; top: 0px; opacity: 0.4; display: none; border-radius: 0px; z-index: 99; right: 1px; height: 156.863px;" class="slimScrollBar"></div><div style="width: 3px; height: 100%; position: absolute; top: 0px; display: none; border-radius: 0px; background: none repeat scroll 0% 0% rgb(51, 51, 51); opacity: 0.2; z-index: 90; right: 1px;" class="slimScrollRail"></div></div>
+                        </li>
+                        <li class="footer"><a href="#">View all</a></li>
+                    </ul>
+                </li> -->
                 <!-- User Account: style can be found in dropdown.less -->
                 <li class="dropdown user user-menu">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                         <i class="glyphicon glyphicon-user"></i>
-                        <span>{{ Sentry::getUser()->username }} <i class="caret"></i></span>
+                        <span>{{ (Sentry::getUser()->first_name.' '.Sentry::getUser()->last_name) or Sentry::getUser()->username }} <i class="caret"></i></span>
                     </a>
                     <ul class="dropdown-menu">
                         <!-- User image -->
                         <li class="user-header bg-light-blue">
                             <img src="{{Gravatar::src(Sentry::getUser()->email, 215)}}" class="img-circle" alt="User Image" />
                             <p>
-                                {{ Sentry::getUser()->username }}
-                                <small>Member since {{ Sentry::getUser()->created_at }}</small>
+                                {{ (Sentry::getUser()->first_name.' '.Sentry::getUser()->last_name) or Sentry::getUser()->username }}
+                                <small>Member since {{ date("F, Y", strtotime(Sentry::getUser()->created_at)) }}</small>
                             </p>
                         </li>
-                        
+
                         <!-- Menu Footer-->
                         <li class="user-footer">
                             <div class="pull-left">
-                                <a href="{{ URL::route('showUser', Sentry::getUser()->id ) }}" class="btn btn-default btn-flat">Profile</a>
+                                <a href="{{ route('showUser', Sentry::getUser()->id ) }}" class="btn btn-default btn-flat">Profile</a>
                             </div>
                             <div class="pull-right">
-                                <a href="{{ URL::route('logout') }}" class="btn btn-default btn-flat">Sign out</a>
+                                <a href="{{ route('logout') }}" class="btn btn-default btn-flat">Sign out</a>
                             </div>
                         </li>
                     </ul>
